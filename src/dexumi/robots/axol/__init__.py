66ba5a55754e91f50215b3f6713576a94fa5ce74
leoperezz <leo.perez.nestares@gmail.com>
2026-06-18 14:12:48 -0500

    adding axol embodiment

---
+++ src/dexumi/robots/axol/__init__.py
@@ -0,0 +1,6 @@
+"""Public re-exports for the Axol kinematics solver."""
+
+from .config import KinematicsConfig
+from .solver import KinematicsSolver
+
+__all__ = ["KinematicsConfig", "KinematicsSolver"]
